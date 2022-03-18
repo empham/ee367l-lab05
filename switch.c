ffd666f1b687c68750cd6050ab498cbb9b9ea6bc
Emily <emilyn3@hawaii.edu>
2022-03-17 15:20:22 -1000

    files for switch
    SWnet.config contains switched network config shown in Figure 1. switch.c and switch.h are just skeletons.

---
+++ switch.c
@@ -0,0 +1,23 @@
+/*
+ * file: switch.c
+ *
+ * code for a switch node
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "main.h"
+#include "net.h"
+#include "man.h"
+#include "host.h"
+#include "packet.h"
+#include "switch.h"
+
+#define MAX_FT_ENTRIES 100 // maximum number of entries in a forwarding table
+
---
+++ switch.h
@@ -0,0 +1,8 @@
+/*
+ * file: switch.h
+ * 
+ * contains prototypes, enums and structs for switch.c
+ */
+
+void switch_main(p_node->id);
+
